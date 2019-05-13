356bc5b96f05332283f449b4b61bde0bc39ce5ff
Wayne Lewis <wlewis@ospreydcs.com>
2019-05-12 22:45:48 -0700

    ENH: Fix variable naming and function defs

--- KS3497xAApp/src/KS3497xA.cpp
+++ KS3497xAApp/src/KS3497xA.cpp
@@ -18,7 +18,7 @@
 
 #define NUM_KS3497xA_PARAMS ((int)(&LAST_KS3497XA_PARAM - &FIRST_KS3497XA_PARAM + 1))
 
-KS3497xA::KS3497xA(const char *portName, int nChannels, int maxBuffers, int maxMemory)
+KS3497xA::KS3497xA(const char *portName)
     : asynPortDriver(portName, 
         1, // maxAddr
         NUM_KS3497xA_PARAMS,
@@ -33,37 +33,37 @@ KS3497xA::KS3497xA(const char *portName, int nChannels, int maxBuffers, int maxM
     const char *functionName = "KS3497xA";
 
     /* Create parameters */
-    createParam(KS3497xASerialNumberString,     asynParamOctet,     &KS3497XASerialNumber);
-    createParam(KS3497xATriggerSourceString,    asynParamInt32,     &KS3497XATriggerSource);
-    createParam(KS3497xANumDataPointsString,    asynParamInt32,     &KS3497XANumDataPoints);
+    createParam(KS3497xASerialNumberString,     asynParamOctet,     &KS3497xASerialNumber);
+    createParam(KS3497xATriggerSourceString,    asynParamInt32,     &KS3497xATriggerSource);
+    createParam(KS3497xANumDataPointsString,    asynParamInt32,     &KS3497xANumDataPoints);
 
 }
 
-KS3497xA::readInt32(asynUser *pasynUser, epicsInt32 *value)
+asynStatus KS3497xA::readInt32(asynUser *pasynUser, epicsInt32 *value)
 {
     asynStatus status = asynSuccess;
     return status;
 }
 
-KS3497xA::writeInt32(asynUser *pasynUser, epicsInt32 value)
+asynStatus KS3497xA::writeInt32(asynUser *pasynUser, epicsInt32 value)
 {
     asynStatus status = asynSuccess;
     return status;
 }
 
-KS3497xA::readFloat64(asynUser *pasynUser, epicsFloat64 *value)
+asynStatus KS3497xA::readFloat64(asynUser *pasynUser, epicsFloat64 *value)
 {
     asynStatus status = asynSuccess;
     return status;
 }
 
-KS3497xA::writeFloat64(asynUser *pasynUser, epicsFloat64 value)
+asynStatus KS3497xA::writeFloat64(asynUser *pasynUser, epicsFloat64 value)
 {
     asynStatus status = asynSuccess;
     return status;
 }
 
-KS3497xA::writeOctet(asynUser *pasynUser, const char *value, size_t maxChars, size_t *nActual)
+asynStatus KS3497xA::writeOctet(asynUser *pasynUser, const char *value, size_t maxChars, size_t *nActual)
 {
     asynStatus status = asynSuccess;
     return status;
--- KS3497xAApp/src/KS3497xA.h
+++ KS3497xAApp/src/KS3497xA.h
@@ -5,9 +5,9 @@
 #include <epicsExport.h>
 #include <asynPortDriver.h>
 
-#define KS3497XASerialNumberString      "SerialNumber"      // asynOctet
-#define KS3497XATriggerSourceString     "TriggerSource"     // asynInt32    
-#define KS3497XANumDataPointsString     "NumDataPoints"     // asynInt32    
+#define KS3497xASerialNumberString      "SerialNumber"      // asynOctet
+#define KS3497xATriggerSourceString     "TriggerSource"     // asynInt32    
+#define KS3497xANumDataPointsString     "NumDataPoints"     // asynInt32    
 
 class KS3497xA : public asynPortDriver {
 
@@ -19,15 +19,14 @@ public:
     virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
     virtual asynStatus readFloat64(asynUser *pasynUser, epicsFloat64 *value);
     virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
-    virtual asynStatus writeOctet (asynUser *pasynUser, const char *value, size_t maxChars, size_t *nActual);
+    virtual asynStatus writeOctet(asynUser *pasynUser, const char *value, size_t maxChars, size_t *nActual);
 
 protected:
-    int P_serialNumber;
-    #define FIRST_KS3497XA_PARAM P_serialNumber;
-    int P_triggerSource;
-    int P_numDataPoints;
-    #define LAST_KS3497XA_PARAM P_numDataPoints;
-    
+    int KS3497xASerialNumber;
+    #define FIRST_KS3497XA_PARAM KS3497xASerialNumber
+    int KS3497xATriggerSource;
+    int KS3497xANumDataPoints;
+    #define LAST_KS3497XA_PARAM KS3497xANumDataPoints
 };
 
 #endif
