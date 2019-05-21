7b79ea0b38afbd1d1de8cf7f4276e60ea1b07f8f
Wayne Lewis <wlewis@ospreydcs.com>
2019-05-20 17:13:14 -0700

    ENH: Move asyn record to separate database

--- iocBoot/iocKS34972A/st.cmd
+++ iocBoot/iocKS34972A/st.cmd
@@ -10,6 +10,9 @@ epicsEnvSet("DEV_IP_ADDRESS", "192.168.1.41")
 epicsEnvSet("DEV_ASYN_PORT", "KS3497XA")
 epicsEnvSet("DEV_SUP_ASYN_PORT", "MUX1")
 
+epicsEnvSet("PREFIX", "MMT:")
+epicsEnvSet("DEVICE", "MUX1:")
+
 cd "${TOP}"
 
 ## Register all support components
@@ -23,7 +26,8 @@ drvAsynIPPortConfigure("$(DEV_ASYN_PORT)",  "$(DEV_IP_ADDRESS):5025", 0, 0, 0)
 initKS3497xA("$(DEV_SUP_ASYN_PORT)", "$(DEV_ASYN_PORT)", 1000)
 
 ## Load record instances
-dbLoadRecords("db/KS3497xA.db","P=MMT:,R=MUX1:,PORT=$(DEV_SUP_ASYN_PORT)")
+dbLoadRecords("db/KS3497xA.db","P=$(PREFIX),R=$(DEVICE),PORT=$(DEV_SUP_ASYN_PORT)")
+dbLoadRecords("db/asynRecord.db","P=$(PREFIX),R=$(DEVICE)Asyn,PORT=$(DEV_ASYN_PORT),OMAX=80,IMAX=80,ADDR=0")
 
 #asynSetTraceMask("$(DEV_ASYN_PORT)", -1, 0xFF)
 asynSetTraceMask("$(DEV_SUP_ASYN_PORT)", -1, 0x11)
